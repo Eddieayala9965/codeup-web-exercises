d47a4ab87a5bcb3532858bad6c69b8b7a9207aab
Eddie Ayala <heriberto.ayala1981@gmail.com>
2023-10-04 08:29:39 -0500

    js2

--- js/js-with-html-lecture.js
+++ js/js-with-html-lecture.js
@@ -1,6 +1,29 @@
 let isLoggedIn = true;
-let isAdmin = true;
+let isAdmin = false
 
-if (isLoggedIn && isAdmin) {
-    console.log('show the buttons');
+let shapes = ['circle', 'rectangle', 'square', 'oval', 'triangle', 'hexagon', 'octagon'];
+let shape = shapes[Math.floor(Math.random()*shapes.length)];
+
+let favorite = 'octagon'; //
+
+if (shape === 'circle') {
+    console.log("Circle is round")
 }
+else if (shape === 'rectangle') {
+    console.log("long square")
+}
+else if (shape === 'square') {
+    console.log("shaped like a box")
+}
+else if (shape === 'triangle') {
+    console.log("pyramid")
+}
+else if (shape === 'octagon') {
+    console.log("UFC")
+}
+else {
+    console.log("idk what shape that is")
+}
+
+(shape === favorite) ? console.log(shape + " is my fav shape") : console.log(" is not my fav shape")
+
