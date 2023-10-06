4f805db07ce2a8383ae885574ced38ccff9a77c5
Eddie Ayala <heriberto.ayala1981@gmail.com>
2023-10-06 18:35:56 -0500

    conditionalsjs

---
+++ js/conditionals-lecture.js
@@ -0,0 +1,61 @@
+
+
+let isLoggedIn = "eddie";
+
+if (isLoggedIn) {
+    console.log("logged in");
+}
+else {
+    console.log("not logged in");
+}
+
+console.log("after conditional");
+
+
+
+// const users = 10000;
+//
+// if (users >= 10000 ) {
+//     console.log("too many");
+// } else if (users >= 200) {
+//     console.log("idk");
+// } else {
+//     console.log("better job please");
+// }
+
+// const userRole = "admin";
+// if (userRole === "student"){
+//     console.log("let them see stuff")
+// }else if (userRole === "admin");
+//     console.log("see portal and stuff");
+// else {
+//     console.log("cant see");
+// }
+//
+// console.log("after conditional");
+// const Log = false;
+// let me = Log ? "welcoe" : "log in";
+// console.log(me);
+
+// let pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// switch (pizzaPreference) {
+//     case "cheese":
+//         console.log("cheese the best");
+//         break;
+//     case "pepperoni":
+//         console.log("dont like it");
+//         break;
+//     case "supreme":
+//         console.log("nahhhh cuh");
+//         break;
+//     case "extra c":
+//         console.log("oh yea the best");
+//         break;
+//     default:
+//         console.log("jdjdjdjdj")
+//         break;
+// }
+// const nrmOfUsers = 147;
+// switch(nrmOfUsers) {
+
