fdb7337987a41bdf3c1740e616a35eea7fcb16a5
Eddie Ayala <heriberto.ayala1981@gmail.com>
2023-11-01 14:44:21 -0500

    resume-page

--- js/more-practice.js
+++ js/more-practice.js
@@ -234,109 +234,109 @@
 //     i * 2
 // } while (i <= 65536);
 // console.log(i)
-(function(){
-    "use strict";
-
-    /**
-     * TODO:
-     * Create an array of 4 people's names and store it in a variable called
-     * 'names'.
-     */
-    const names = ['eddie', 'yesenia', 'skylor', 'brandon']
-
-    /**
-     * TODO:
-     * Create a log statement that will log the number of elements in the names
-     * array.
-     */
-const nmrOfNames = names.length
-    console.log(names.length)
-
-    /**
-     * TODO:
-     * Create log statements that will print each of the names individually by
-     * accessing each element's index.
-     */
-    console.log(names[0])
-    console.log(names[1])
-    console.log(names[2])
-    console.log(names[3])
-
-    /**
-     * TODO:
-     * Write some code that uses a for loop to log every item in the names
-     * array.
-     */
-
-    for (let i = 0; i <= nmrOfNames ;i++) {
-        console.log(i)
-    }
-
-
-    /**
-     * TODO:
-     * Refactor your above code to use a `forEach` loop
-     */
-
-    names.forEach((name, i) => {
-        console.log(`Name #${i + 1} is ${name}`)
-    })
-
-    /**
-     * TODO:
-     * Create the following three functions, each will accept an array and
-     * return an an element from it
-     * - first: returns the first item in the array
-     * - second: returns the second item in the array
-     * - last: returns the last item in the array
-     *
-     * Example:
-     *  > first([1, 2, 3, 4, 5]) // returns 1
-     *  > second([1, 2, 3, 4, 5]) // returns 2
-     *  > last([1, 2, 3, 4, 5]) // return 5
-     */
-
-    const first = (array) => {
-        let result = array[1]
-        return result
-    }
-
-    const second = (array) => {
-        let result = array[2]
-        return result
-    }
-    const third = (array) => {
-        let result = array[3]
-        return result
-    }
-    console.log(first(names))
-    console.log(second(names))
-    console.log(third(names))
-
-})();
+// (function(){
+//     "use strict";
+//
+//     /**
+//      * TODO:
+//      * Create an array of 4 people's names and store it in a variable called
+//      * 'names'.
+//      */
+//     const names = ['eddie', 'yesenia', 'skylor', 'brandon']
+//
+//     /**
+//      * TODO:
+//      * Create a log statement that will log the number of elements in the names
+//      * array.
+//      */
+// const nmrOfNames = names.length
+//     console.log(names.length)
+//
+//     /**
+//      * TODO:
+//      * Create log statements that will print each of the names individually by
+//      * accessing each element's index.
+//      */
+//     console.log(names[0])
+//     console.log(names[1])
+//     console.log(names[2])
+//     console.log(names[3])
+//
+//     /**
+//      * TODO:
+//      * Write some code that uses a for loop to log every item in the names
+//      * array.
+//      */
+//
+//     for (let i = 0; i <= nmrOfNames ;i++) {
+//         console.log(i)
+//     }
+//
+//
+//     /**
+//      * TODO:
+//      * Refactor your above code to use a `forEach` loop
+//      */
+//
+//     names.forEach((name, i) => {
+//         console.log(`Name #${i + 1} is ${name}`)
+//     })
+//
+//     /**
+//      * TODO:
+//      * Create the following three functions, each will accept an array and
+//      * return an an element from it
+//      * - first: returns the first item in the array
+//      * - second: returns the second item in the array
+//      * - last: returns the last item in the array
+//      *
+//      * Example:
+//      *  > first([1, 2, 3, 4, 5]) // returns 1
+//      *  > second([1, 2, 3, 4, 5]) // returns 2
+//      *  > last([1, 2, 3, 4, 5]) // return 5
+//      */
+//
+//     const first = (array) => {
+//         let result = array[1]
+//         return result
+//     }
+//
+//     const second = (array) => {
+//         let result = array[2]
+//         return result
+//     }
+//     const third = (array) => {
+//         let result = array[3]
+//         return result
+//     }
+//     console.log(first(names))
+//     console.log(second(names))
+//     console.log(third(names))
+//
+// })();
 
 // Of course! Here are some exercises related to arrays and loops for you to practice. Try to solve each exercise on your own, and then check your solutions:
 //
 //     **Exercise 1: Array Sum**
 // Write a function that takes an array of numbers and returns the sum of all the elements.
-
-const numbers = [1, 2, 3, 4, 5];
-let sum = 0;
-
-numbers.forEach((number) => {
-    sum += number
-})
-
-console.log(sum)
-// **Exercise 2: Even Numbers**
-// Write a function that takes an array of numbers and returns a new array containing only the even numbers from the original array.
-
-const isEven = [1, 2, 3, 4, 5, 6];
-
-   const even = (array) => {
-    let result = array[1]
-   }
-console.log()
+//
+// const numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+//
+// numbers.forEach((number) => {
+//     sum += number
+// })
+//
+// console.log(sum)
+// // **Exercise 2: Even Numbers**
+// // Write a function that takes an array of numbers and returns a new array containing only the even numbers from the original array.
+//
+// const isEven = [1, 2, 3, 4, 5, 6];
+//
+//    const even = (array) => {
+//     let result = array[1]
+//    }
+// console.log()
 
 // **Exercise 3: Maximum Value**
 // Write a function that takes an array of numbers and returns the maximum value in the array.
@@ -350,16 +350,56 @@ console.log()
 
 // **Exercise 5: Average of Array Elements**
 // Write a function that takes an array of numbers and returns the average (mean) of the elements.
+
 //
+// let number = [1, 2, 3, 4 , 5, 6]
+//
+// const aveArray = (arr) => {
+//     let sum = arr.reduce((acc, currentValue) => acc + currentValue, 0)
+//     return sum / arr.length
+//
+// }
+//
+// console.log(aveArray(number))
+
 // **Exercise 6: Find Index of Element**
 // Write a function that takes an array and an element to find. It should return the index of the first occurrence of the element in the array or -1 if the element is not found.
+
+// let number = ["ed", "edd", "eddie"]
 //
+// const indexArray = (arr) => {
+//     let index = arr.indexOf("eddie")
+//     return index
+// }
+// let result = indexArray(number)
+// console.log(result)
+
 // **Exercise 7: Remove Duplicates**
 // Write a function that takes an array and removes any duplicate elements, returning a new array with unique values.
-//
+
+// let number = ["ed", "edd", "eddie", "eddie"]
+// const dupArray = (arr) => {
+//     let dup = arr.splice(3, "eddie")
+//     return dup
+// }
+// console.log(dupArray(number))
+
 // **Exercise 8: FizzBuzz**
 // Write a program that prints the numbers from 1 to 100. For multiples of 3, print "Fizz." For multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
-//
+
+// const fizzBuzz = (num, num2) => {
+//     for ()
+//     if (num && num2 % 3 === 0) {
+//         return "Fizz"
+//     } else if ( num && num2 % 5 === 0) {
+//         return "Buzz"
+//     }
+// }
+// console.log(fizzBuzz(1, 100))
+
+
+
+
 // **Exercise 9: Array Rotation**
 // Write a function that takes an array and a number, and rotates the array to the right by that number of positions.
 //
@@ -367,3 +407,52 @@ console.log()
 // Write a function that takes a string and checks if it's a palindrome (reads the same forwards and backward).
 //
 // These exercises cover a range of array and loop concepts and should help you become more proficient in working with arrays and loops in JavaScript. Enjoy practicing!
+
+// let number = [5, 8, 9, 4, 2,]
+
+// function digitialRoot(n) {
+//     if (typeof parseFloat(n !== "number")){
+//
+//     }
+//
+//
+//     let numArr = n.toString().split("")
+//     let result;
+//     while (numArray.length > 1) {
+//         result = 0
+//         for (let number of numArr) {
+//             result+=parseFloat(number)
+//         }
+//         // loop through num
+//             // add the iteration to result
+// numArr = result.toString().split("")
+//
+//     }
+//     result = parseFloat(numArray.join(""));
+//     return result
+// }
+// console.log(digitialRoot())
+
+// let count = 0;
+// let max = 10;
+// let interval = 1000; // interval time in milliseconds
+//
+// const intervalId = setInterval( () => {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
+
+// const delay = 5000; // delay time in milliseconds
+//
+// const timeoutId = setTimeout(() => alert('Here is a delayed hello!'), delay);
+//
+// // to cancel the timeout, you can call
+// clearTimeout(timeoutId);
+// // prior to the delay expiring
+
+
--- js/practice-work-js.js
+++ js/practice-work-js.js
@@ -17,29 +17,28 @@
 //     **Exercise 1: Counting Numbers**
 // Write a loop that prints the numbers from 1 to 15.
 
-// for (i = 0;i <= 15 ;i++){
-//     console.log(i)
-// }
+
 
 // **Exercise 2: Even Numbers**
 // Write a loop that prints the even numbers from 2 to 12 (2, 4, 6, 8, 10, 12).
 
-// for (let i = 2;i <= 12 ;i+=2){
+// for (let i = 2 ; i <= 12  ;i+=2 ){
 //     console.log(i)
 // }
 
 // **Exercise 3: Odd Numbers**
 // Write a loop that prints the odd numbers from 1 to 11 (1, 3, 5, 7, 9, 11).
 
-// for (let i = 1 ; i <= 11 ;i+=2){
+// for (let i = 1 ; i <= 11 ;i += 2){
 //     console.log(i)
 // }
 
 // **Exercise 4: Multiplication Table**
 // Write a loop to print the multiplication table for the number 4 from 1 to 8 (e.g., 4 x 1 = 4, 4 x 2 = 8, ...).
 //
-// for (let i = 4 ; i <= 8 ;i++) {
-//     console.log(`4 x ${i} = ${i * 4}`)
+
+// for (i = 4 ;i<=10 ;i++){
+//    console.log(`4 x ${i} = ${i * 4}`)
 // }
 
 // **Exercise 5: Sum of Numbers**
@@ -55,17 +54,17 @@
 // **Exercise 6: Reverse Counting**
 // Write a loop to count backward from 15 to 5.
 
-// for (let i = 15; i >= 5 ;i-- ){
+// for (i = 15; i >= 5; i--)
 //     console.log(i)
-// }
 
 // **Exercise 7: Custom Greeting**
 // Write a loop that asks the user for their name and prints a personalized greeting 3 times.
 
-// for (i = 0;i < 3 ; i++){
-//     let name= prompt("what is yo name?")
-//     console.log(`Hello ${name}`)
+// for (let i = 0 ;i <  3; i++){
+//     let name = prompt("what is your name")
+// console.log(`hello, ${name}`)
 // }
+
 
 // **Exercise 8: Square Numbers**
 // Write a loop to print the squares of the numbers from 1 to 6 (1, 4, 9, 16, 25, 36).
@@ -81,25 +80,108 @@
 //
 //     **Question 1: Array Sum**
 // Write a function that takes an array of numbers as an argument and returns the sum of all the numbers in the array.
+// const array = (arr) => {
+//     let numbers = [1, 2, 3, 4, 5, 6, 6, 7, 7]
+//     arr = numbers
+//     let sum = arr.reduce((acc, current) => acc + current, 0)
+//     return sum
 //
+// }
+// console.log(array())
+
+
+// }
+// const nmrArray = [1, 2, 3, 4 ,5]
+// let sum = sumArray(nmrArray)
+// console.log(sum)
+
 // **Question 2: Find Maximum Value**
 // Write a function that finds and returns the maximum value in an array of numbers.
+
+// const maxArray = (arr) => {
+//     return Math.max(...arr);
+// }
+// const nmrArray = [1, 2, 3, 4, 5];
+// let max = maxArray(nmrArray)
+// console.log(max)
+
+// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let max = Math.max(...number)
+// console.log(max)
 //
+
+// Question 1: Array Average
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+// const AveA = (arr) => {
+//     // let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+//     // arr = numbers
+//     let ave = arr.reduce((acc, currentValue) => acc + currentValue, 0)
+//     let average = ave / arr.length
+//     console.log(average)
+// }
+// console.log(AveA(numbers))
+
+
+// console.log(parse)
+//     Question 2: Find Minimum Value
+// Write a function that finds and returns the minimum value in an array of numbers.
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+// const minArray = (arr) => {
+//     return Math.min(...arr)
+// }
+// let min = minArray(numbers)
+// console.log(min)
+
+//     These modified questions still involve working with arrays and require you to perform slightly different operations. Feel free to tackle these new challenges!
+
+
+
 // **Question 3: Find Minimum Value**
 // Write a function that finds and returns the minimum value in an array of numbers.
-//
+
+
+
+
 // **Question 4: Reverse an Array**
 // Write a function that takes an array and returns a new array with its elements in reverse order.
+
+
+// let names = ["Eddie", "Yesenia", "Aiden", "Skylor"];
 //
+// const revArray = (arr) => {
+//     return arr.reverse().join(" ,");
+// }
+//
+// console.log(revArray(names));
+
+
+
 // **Question 5: Remove Duplicates**
 // Write a function that takes an array and removes any duplicate elements, returning a new array with unique values.
+
 //
+// let names = ["Eddie", "Yesenia", "Aiden", "Skylor", "Eddie"];
+// const removeArray = (arr) => {
+//     let slice = arr.splice()
+//     let result = slice.toString()
+//     return result
+// }
+// console.log(removeArray(names))
+//
+
 // **Question 6: Count Occurrences**
 // Write a function that takes an array and a value and counts how many times that value appears in the array.
-//
+
+
+
+
 // **Question 7: Merge Arrays**
 // Write a function that takes two arrays and merges them into a single array.
-//
+
+
+
+
 // **Question 8: Find Index of Element**
 // Write a function that takes an array and an element to find. It should return the index of the first occurrence of the element in the array or -1 if the element is not found.
 //
@@ -111,34 +193,34 @@
 //
 //     These questions cover a range of fundamental array operations in JavaScript and should provide you with a good opportunity to practice working with arrays.
 
-const createPairs = (arr) => {
-    let len = arr.length
-    let array = []
-    for (let i = 0; i < arr.length; i += 2) {
-        let pairs = []
-        pairs.push(arr[i])
-        pairs.push(arr[i +1])
-        array.push(pairs)
-    }
-    console.log(arr)
-}
-
-const students = ["Amanda",
-    "Bianca",
-    "Cameron",
-    "Chris",
-    "Dany",
-    "David",
-    "Diana",
-    "Drew",
-    "Evan",
-    "Hassan",
-    "Jack",
-    "Jason",
-    "John",
-    "Jonathan",
-    "Jordan",
-    "Joshua",
-    "Katie",
-    "Luis"]
-
+// const createPairs = (arr) => {
+//     let len = arr.length
+//     let array = []
+//     for (let i = 0; i < arr.length; i += 2) {
+//         let pairs = []
+//         pairs.push(arr[i])
+//         pairs.push(arr[i +1])
+//         array.push(pairs)
+//     }
+//     console.log(arr)
+// }
+//
+// const students = ["Amanda",
+//     "Bianca",
+//     "Cameron",
+//     "Chris",
+//     "Dany",
+//     "David",
+//     "Diana",
+//     "Drew",
+//     "Evan",
+//     "Hassan",
+//     "Jack",
+//     "Jason",
+//     "John",
+//     "Jonathan",
+//     "Jordan",
+//     "Joshua",
+//     "Katie",
+//     "Luis"]
+//
