a11a5bd88e6617e3c01ceba4af19a177a1768abf
Eddie Ayala <heriberto.ayala1981@gmail.com>
2023-11-06 13:45:27 -0600

    bom

---
+++ js/BOM-Lecture.js
@@ -0,0 +1,89 @@
+//IIFE
+
+// (()=> {
+//
+// })();
+
+
+
+(()=> {
+
+
+
+                                                            // setIntervalMethod
+
+
+
+
+// let countdown = 11
+//     //this takes in two arguements, first annomyes function, second how often do you want execute the code
+//     const countdownTimer = setInterval(()=>{
+//         countdown--;
+//         console.log(countdown)
+//         if (countdown === 0) {
+//             clearInterval(countdownTimer)
+//             console.log("all Done")
+//         }
+//     }, 1000);
+// console.log("")
+
+
+
+
+
+                                                // setTimeout this just waits before it starts the function
+
+    // const asPopup = setTimeout(()=> {
+    //     alert("YO!")
+    // } , 1000);
+    //
+    //                                             // clearTimeout
+    // const noAdsBtn = document.querySelector('button');
+    // noAdsBtn.addEventListener("click", () => {
+    //     clearTimeout(asPopup)
+    // })
+
+                                                // Location object is a porperty not method
+
+//     console.log(window.location);
+//    const stop = setTimeout(()=>{
+//         window.location.reload()
+//     }, 1000)
+// clearTimeout(stop)
+
+})();
+
+
+
+
+
+
+
+
+// let count = 0;
+// let max = 10;
+// let interval = 1000; // interval time in milliseconds
+//
+// const intervalId = setInterval( () => {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
+
+
+
+// const delay = 5000; // delay time in milliseconds
+//
+// const timeoutId = setTimeout(() => alert('Here is a delayed hello!'), delay);
+//
+// // to cancel the timeout, you can call
+// // clearTimeout(timeoutId);
+// // prior to the delay expiring
+
+
+
+
