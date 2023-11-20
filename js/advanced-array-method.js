2db6ad8aedc715222bb0e83a23f1a068a30ffa27
Eddie Ayala <heriberto.ayala1981@gmail.com>
2023-11-20 14:27:36 -0600

    advanced-array-methods

---
+++ js/advanced-array-method.js
@@ -0,0 +1,5 @@
+import {NETFLIX_TITLES } from "./netflix-titles";
+
+
+// querySelector is the find method basically
+// querySelectorAll is the filter all method basically
---
+++ js/map-filter-reduce.js
@@ -0,0 +1,86 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const userLanguages = {}
+for (let user of users) {
+   if (user.languages.length >= 3) {
+       userLanguages[user.name] = user.languages
+
+   }
+}
+console.log(userLanguages)
+
+const userEmails = users.map((user)=>{
+  return user.email
+})
+console.log(userEmails)
+
+
+const totalYears = users.reduce((accumalater, year)=>{
+return accumalater + year.yearsOfExperience
+
+}, 0)
+const averageYears = totalYears / users.length
+console.log("Average =>",averageYears)
+console.log("Total =>", totalYears)
+
+const longestEmail = users.reduce((longest, user ) => {
+    if (user.email.length > longest.length) {
+        longest = user.email;
+    }
+    return longest;
+}, "")
+console.log(longestEmail)
+
+
+const listOfNames = users.reduce((acc, user )=>{
+    return acc + user.name + ", "
+}, "")
+console.log(listOfNames)
+
+const languages = users.reduce((acc, user)=> {
+    for (let language of user.languages) {
+        if (!acc.includes(language)) {
+            acc.push(language)
+        }
+    }
+    return acc
+}, [0])
+
+console.log(languages)
+
